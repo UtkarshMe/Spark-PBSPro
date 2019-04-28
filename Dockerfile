72bedc41a976a39412e1898f0163ce9aa24d3ed0
Utkarsh Maheshwari <coditva@gmail.com>
2019-04-28 13:02:45 +0530

    Deploy docker test

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM pbspro/pbspro
 
+## Install dependencies
 RUN yum install -y java-1.8.0-openjdk-devel java-1.8.0-openjdk
 ENV JAVA_HOME /usr/lib/jvm/java-1.8.0-openjdk/
 
+## Add spark project to image and configure it for PBSPro
 ADD . /opt/spark
-WORKDIR /opt/spark 
-
+WORKDIR /opt/spark
 RUN echo "spark.pbs.executor.home /opt/spark" >> conf/spark-defaults.conf
 RUN echo "spark.master pbs" >> conf/spark-defaults.conf
--- dev/deploy_docker.sh
+++ dev/deploy_docker.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+image="spark-with-pbspro"
+tag="nightly"
+
 quit() {
   echo "$1"
   exit 1
 }
 
+# build image
 cd ../../
-sudo docker build -f resource-managers/pbs/Dockerfile .
+sudo docker build -t $image:$tag -f resource-managers/pbs/Dockerfile . \
+    || quit "Could not build docker image"
+
+# push to docker hub
+sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD \
+    || quit "Could not login to docker"
+sudo docker push $image:$tag \
+    || quit "Could not push to docker hub"
